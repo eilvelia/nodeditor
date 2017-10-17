5c5a3067fbdf8660dc8a738e2b26386c6744bab5
save14 <save999999@gmail.com>
2017-10-17 23:14:48 +0300

    add index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// @flow
+
+import pkg from '../package.json'
+import program from 'commander'
+import keypress from 'keypress'
+import readline from 'readline'
+import editor from './editor'
+
+program
+  .version(pkg.version)
+  .parse(process.argv)
+
+keypress(process.stdin)
+
+readline.cursorTo(process.stdin, 0, 0)
+readline.clearScreenDown(process.stdin)
+
+// $FlowFixMe
+process.stdin.setRawMode(true)
+
+process.on('exit', () => {
+  readline.cursorTo(process.stdin, 0, 0)
+  readline.clearScreenDown(process.stdin)
+})
+
+process.stdin.on('keypress', editor.keypress)
