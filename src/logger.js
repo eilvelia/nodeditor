a62732517a2e1d8fcf5a06689c79eac7a3a40344
save14 <save999999@gmail.com>
2017-10-27 05:34:32 +0300

    Update logger

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,21 @@
 // @flow
 
 import fs from 'fs'
+import fse from 'fs-extra'
+import path from 'path'
 import { EOL } from 'os'
+import persistPath from 'persist-path'
+import pkg from '../package.json'
 
 const LOG_FILE = 'latest.log'
 
+const ppath = persistPath(`${pkg.name}`)
+
+fse.mkdirpSync(ppath)
+
 const stream = process.env.NODE_ENV === 'production'
   ? null
-  : fs.createWriteStream(LOG_FILE)
+  : fs.createWriteStream(path.join(ppath, LOG_FILE))
 
 const log = (...args: Array<*>) => {
   if (!stream) return
